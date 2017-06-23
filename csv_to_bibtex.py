1ed135ff98ea3a841ebe071864663b08e8b67daf
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-06-23 14:18:00 -0400

    Changes for Zotero parsing

--- csv_to_bibtex.py
+++ csv_to_bibtex.py
@@ -6,12 +6,14 @@ import csv
 import sys
 import argparse
 import textwrap
+import numpy as np
 from nltk.corpus import stopwords
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 
 
 STOP_WORDS = stopwords.words('english')
+MAX_ENTRIES = 9999999999
 
 HEADERS = {
     'Abstract Note': 'abstract',
@@ -150,7 +152,7 @@ def fix_columns_headers(old_row):
     new_row = {}
 
     for old_key, value in old_row.items():
-        if re.match(r'(x.*)?key', old_key, re.IGNORECASE):
+        if re.match(r'(.*)?key$', old_key, re.IGNORECASE):
             old_key = 'key'
 
         new_key = old_key.replace('.', ' ').strip()
@@ -159,10 +161,31 @@ def fix_columns_headers(old_row):
     return new_row
 
 
+def get_filter_rows(args):
+    """Set up a dict with all of the rows that will be filtered out."""
+
+    filter_rows = {}
+
+    if not args.filter_csv:
+        return filter_rows
+
+    with open(args.filter_csv) as csv_file:
+        reader = csv.DictReader(csv_file)
+
+        for row in reader:
+            row = fix_columns_headers(row)
+            key = entry_key(row)
+            filter_rows[key] = 1
+
+    return filter_rows
+
+
 def parse_csv_file(args):
     """Parse a CSV file into bibtex format."""
 
-    bibtex_db = BibDatabase()
+    entries = []
+
+    filter_rows = get_filter_rows(args)
 
     with open(args.csv_file) as csv_file:
         reader = csv.DictReader(csv_file)
@@ -174,6 +197,10 @@ def parse_csv_file(args):
 
             row = fix_columns_headers(row)
 
+            key = entry_key(row)
+            if filter_rows.get(key):
+                continue
+
             if row['Item Type'] not in TYPES:
                 print('ItemType not found: "{}"'.format(row['Item Type']))
                 sys.exit()
@@ -182,12 +209,25 @@ def parse_csv_file(args):
 
             entry = add_entry(row, row_type['type'], row_type['remap'])
 
-            bibtex_db.entries.append(entry)
+            entries.append(entry)
 
-    writer = BibTexWriter()
-    writer.order_entries_by = None
-    with open(args.bibtex_file, 'w') as bibtex_file:
-        bibtex_file.write(writer.write(bibtex_db))
+    if args.randomize:
+        entries = np.random.permutation(entries)
+
+    for i, beg in enumerate(range(0, len(entries), args.max_entries), 1):
+        file_name = args.bibtex_file
+        if args.max_entries != MAX_ENTRIES:
+            root, ext = os.path.splitext(file_name)
+            file_name = '{}{}{}'.format(root, i, ext)
+
+        print(i, len(entries[beg:beg + args.max_entries]))
+
+        bibtex_db = BibDatabase()
+        bibtex_db.entries = entries[beg:beg + args.max_entries]
+        writer = BibTexWriter()
+        writer.order_entries_by = None
+        with open(file_name, 'w') as bibtex_file:
+            bibtex_file.write(writer.write(bibtex_db))
 
 
 def parse_command_line():
@@ -207,6 +247,15 @@ def parse_command_line():
 
     group.add_argument('-b', '--bibtex-file', required=True, metavar='BIBTEX',
                        help='Output Bibtex file.')
+
+    group.add_argument('-f', '--filter-csv', metavar='FILTER',
+                       help='Filter CSV rows by rows in this file.')
+
+    group.add_argument('-m', '--max-entries', default=MAX_ENTRIES, type=int,
+                       help='Maximum entries per bibtex output file.')
+
+    group.add_argument('-r', '--randomize', action='store_true',
+                       help='Shuffle the bibtext output entries?')
 
     group.add_argument('-s', '--starting-row', metavar='ROW', default=0,
                        type=int,
